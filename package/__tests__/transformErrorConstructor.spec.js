06b5d930c91cf3ac127a6a221f0075d7ad243d0d
vitor <vi.souza.almeida@protonmail.com>
2024-03-17 13:19:14 -0300

    refactor(jest): move to jest

--- package/__tests__/transformErrorConstructor.spec.js
+++ package/__tests__/transformErrorConstructor.spec.js
@@ -1,53 +1,27 @@
-import { describe, it } from "vitest";
 const { RuleTester } = require("eslint");
 const { rules } = require("../index");
 
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
 
-describe("transformErrorConstructor", () => {
-  it("transform Error constructor to Meteor.Error", () => {
-    ruleTester.run(
-      "transformErrorConstructor",
-      rules["transform-error-constructor"],
+ruleTester.run(
+  "transformErrorConstructor",
+  rules["transform-error-constructor"],
+  {
+    valid: [
+      { code: "throw new Meteor.Error('error')" },
+      { code: "class CustomError extends Meteor.Error{}" },
+    ],
+    invalid: [
       {
-        valid: [
-          {
-            code: "throw new Meteor.Error('error')",
-          },
-        ],
-        invalid: [
-          {
-            code: "throw new Error('error')",
-            errors: [{ message: "Use 'Meteor.Error' instead of 'Error'." }],
-            output: "throw new Meteor.Error('error')",
-          },
-        ],
-      }
-    );
-  });
-
-  it("transform Error constructor to Meteor.Error in class declaration", () => {
-    ruleTester.run(
-      "transformErrorConstructor",
-      rules["transform-error-constructor"],
+        code: "throw new Error('error')",
+        errors: [{ message: "Use 'Meteor.Error' instead of 'Error'." }],
+        output: "throw new Meteor.Error('error')",
+      },
       {
-        valid: [
-          {
-            code: "class CustomError extends Meteor.Error{}",
-          },
-        ],
-        invalid: [
-          {
-            code: "class CustomError extends Error{}",
-            errors: [
-              {
-                message: "Extend 'Meteor.Error' instead of 'Error'.",
-              },
-            ],
-            output: "class CustomError extends Meteor.Error{}",
-          },
-        ],
-      }
-    );
-  });
-});
+        code: "class CustomError extends Error{}",
+        errors: [{ message: "Extend 'Meteor.Error' instead of 'Error'." }],
+        output: "class CustomError extends Meteor.Error{}",
+      },
+    ],
+  }
+);
